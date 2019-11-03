d6a6260199d6b0af821947999ce5006c4e8db669
Chieko Kanematsu <c.kanematsu2018@gmail.com>
2019-11-03 10:57:34 +0900

    .gitignor　使ったよ

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+def fizzbuzz_convert(number):
+
+    if number % 15 == 0:
+        print ('FizzBuzz')
+    if number % 3 == 0:
+        print ('Fizz')
+    if number % 5 == 0:
+        print ('Buzz')
+    else:
+        print(number)
+#to do
+
+assert fizzbuzz_convert(3) == 'Fizz'
+assert fizzbuzz_convert(5) == 'Buzz'
+assert fizzbuzz_convert(15) == 'FizzBuzz'
