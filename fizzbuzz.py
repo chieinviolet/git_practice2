07670aae2e78e138087a4ed5992c95d612b934e8
Chieko Kanematsu <c.kanematsu2018@gmail.com>
2019-11-03 11:33:38 +0900

    fizzbuzz作ったよ

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,18 @@
 def fizzbuzz_convert(number):
 
     if number % 15 == 0:
-        print ('FizzBuzz')
+        return ('FizzBuzz')
     if number % 3 == 0:
-        print ('Fizz')
+        return ('Fizz')
     if number % 5 == 0:
-        print ('Buzz')
-    else:
-        print(number)
+        return ('Buzz')
+    return str(number)
+
+
+for i in range(1, 101):
+    print(fizzbuzz_convert(i))
 
 assert fizzbuzz_convert(3) == 'Fizz'
 assert fizzbuzz_convert(5) == 'Buzz'
 assert fizzbuzz_convert(15) == 'FizzBuzz'
+assert fizzbuzz_convert(7) == '7'
